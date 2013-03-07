35ebd0f87ddc8aadbb0bfb1f2598316fcaad358a
Igor Mitsyanko <i.mitsyanko@samsung.com>
2013-03-07 22:04:27 +0400

    hw/exynos4210.c: set chipid_and_omr array size to TARGET_PAGE_SIZE
    During initialization, memory region size is aligned to page size,
    but size of chipid_and_omr array is less then TARGET_PAGE_SIZE. This could result
    in errors in some cases, specifically, it could cause errors during VM migration.
    Set chipid_and_omr array size to TARGET_PAGE_SIZE.
    
    Signed-off-by: Igor Mitsyanko <i.mitsyanko@samsung.com>

--- hw/arm/exynos4210.c
+++ hw/arm/exynos4210.c
@@ -76,7 +76,7 @@
 /* EHCI */
 #define EXYNOS4210_EHCI_BASE_ADDR           0x12580000
 
-static uint8_t chipid_and_omr[] = { 0x11, 0x02, 0x21, 0x43,
+static uint8_t chipid_and_omr[TARGET_PAGE_SIZE] = { 0x11, 0x02, 0x21, 0x43,
                                     0x09, 0x00, 0x00, 0x00 };
 
 void exynos4210_write_secondary(ARMCPU *cpu,
