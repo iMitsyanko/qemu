c70536ef8b0106656c60169e83312daaabbfcba9
Igor Mitsyanko <i.mitsyanko@gmail.com>
2013-05-10 00:46:32 +0400

    sd.c: use callbacks as a flag to use async IO
    This is temporary to distinguish between sync and async users.
    
    Signed-off-by: Igor Mitsyanko <i.mitsyanko@gmail.com>

--- hw/sd/sd.c
+++ hw/sd/sd.c
@@ -530,6 +530,10 @@ void sd_set_cb(SDState *sd, qemu_irq readonly, qemu_irq insert,
     sd->datbusy_cb = endbusy;
     qemu_set_irq(readonly, sd->bdrv ? bdrv_is_read_only(sd->bdrv) : 0);
     qemu_set_irq(insert, sd->bdrv ? bdrv_is_inserted(sd->bdrv) : 0);
+
+    if (sd->start_bit_cb && sd->datbusy_cb) {
+        sd->iov.iov_base = sd->buf;
+    }
 }
 
 static void sd_erase(SDState *sd)
